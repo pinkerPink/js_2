81b2de551be34a691a16990484b4199ed0b4b3b0
harry_toOn <45828411+vinny5560@users.noreply.github.com>
2020-10-31 23:13:22 +0300

    third lesson

--- js/script.js
+++ js/script.js
@@ -75,4 +75,29 @@ window.addEventListener("DOMContentLoaded", function () {
   }
 
   setClock("timer", deadline);
+
+  let more = document.querySelector(".more");
+  let overlay = document.querySelector(".overlay");
+  let close = document.querySelector(".popup-close");
+  let descriptionBtn = document.querySelectorAll(".description-btn");
+
+  more.addEventListener("click", function () {
+    overlay.style.display = "block";
+    this.classList.add("more-splash");
+    document.body.style.overflow = "hidden";
+  });
+
+  close.addEventListener("click", function () {
+    overlay.style.display = "none";
+    more.classList.remove("more-splash");
+    document.body.style.overflow = "";
+  });
+
+  for (let i = 0; i < descriptionBtn.length; i++) {
+    descriptionBtn[i].addEventListener("click", function () {
+      overlay.style.display = "block";
+      this.classList.add("more-splash");
+      document.body.style.overflow = "hidden";
+    });
+  }
 });
